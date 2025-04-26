b5e86dd3609899d67b0b5873942c8db9d8049971
Dodilanne <dodilanne@ada-anvil.io>
2025-04-26 17:26:30 -0400

    Handle errors and add defaults

---
+++ src/lib/defaults.ts
@@ -0,0 +1,8 @@
+import type { Location } from "./types";
+
+export const defaultPath = __dirname;
+export const defaultFile = "dump.json";
+export const defaultLocation: Location = {
+  path: defaultPath,
+  file: defaultFile,
+};
--- src/node.ts
+++ src/node.ts
@@ -1,28 +1,35 @@
 import { existsSync, readFileSync, rmSync, writeFileSync } from "node:fs";
 import { createDump } from "./lib/create-dump";
+import { defaultLocation } from "./lib/defaults";
 import type { Json, Storage } from "./lib/types";
 
 export const nodeStorage: Storage = {
   write(json: Json, path: string): void {
-    writeFileSync(path, JSON.stringify(json));
+    try {
+      writeFileSync(path, JSON.stringify(json));
+    } catch {}
   },
   read(path: string): Json {
-    const res = readFileSync(path, { encoding: "utf8" });
-    return JSON.parse(res);
+    if (!existsSync(path)) {
+      return {};
+    }
+    try {
+      const res = readFileSync(path, { encoding: "utf8" });
+      return JSON.parse(res);
+    } catch {
+      return {};
+    }
   },
   clear(path: string): void {
     if (existsSync(path)) {
-      rmSync(path);
+      try {
+        rmSync(path);
+      } catch {}
     }
   },
 };
 
-export const DEFAULT_FILE = "dump.json";
-
 export const dump = createDump({
   storage: nodeStorage,
-  location: {
-    path: __dirname,
-    file: DEFAULT_FILE,
-  },
+  location: defaultLocation,
 });
