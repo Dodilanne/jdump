6902a755a283dc622f7d3413232fa72dac19ddf6
Dodilanne <dodilanne@ada-anvil.io>
2025-04-26 13:59:56 -0400

    Create lib skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export {};
+export function dumpWrite(file: string) {}
+
+export function dumpRead(file: string) {}
+
+export function dumpClear(file: string) {}
+
+export function createDump(file: string) {
+  const dump = () => dumpWrite(file);
+  dump.write = () => dumpWrite(file);
+  dump.read = () => dumpRead(file);
+  dump.clear = () => dumpClear(file);
+  return dump;
+}
+
+export function getDefaultFile() {
+  return "";
+}
+
+export const dump = createDump(getDefaultFile());
